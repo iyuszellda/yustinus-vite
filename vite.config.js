3397497267ac278688bc0b3cd296f7f5c3bff5c1
yustinus margiyuna <yustinus@ilcs.co.id>
2025-06-02 15:30:59 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import viteCompression from "vite-plugin-compression";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
