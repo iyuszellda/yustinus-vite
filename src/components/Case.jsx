77ae9f1d03245d00a8cae9f7d483f441e9db9c88
yustinus margiyuna <iyuszellda@gmail.com>
2025-06-03 15:15:08 +0700

    mobile view and etc

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 export default function Case({ children }) {
     return (
-        <div className="grid grid-cols-12">
+        <div className="md:grid md:grid-cols-12 lg:grid lg:grid-cols-12">
             <section className="col-span-10 col-start-2">{children}</section>
         </div>
     );
