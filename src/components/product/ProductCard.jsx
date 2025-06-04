fbdec79385ab9e677728d04421348942d335076f
yustinus margiyuna <iyuszellda@gmail.com>
2025-06-04 15:16:41 +0700

    light dark mode on mobile, also fixing navbar on mobile

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -10,11 +10,11 @@ export default function ProductCard({ product }) {
                         src={product.images[0]}
                         alt="Sunset in the mountains"
                     />
-                    <div className="hover:bg-neutral-700 transition duration-300 absolute bottom-0 top-0 right-0 left-0 opacity-25"></div>
+                    <div className="hover:bg-neutral-700 transition duration-300 absolute opacity-25"></div>
                     <div className="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
                         <span className="font-bold">$ {product.price}</span>
                     </div>
-                    <div className="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                    <div className="hidden md:flex lg:flex text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
                         <span className="font-bold">
                             <small>{product.category.slug}</small>
                         </span>
@@ -24,13 +24,14 @@ export default function ProductCard({ product }) {
             <div className="px-6 py-3">
                 <Link
                     to={`/demo/product/detail/${product.id}`}
-                    className="font-semibold text-sm text-neutral-950 dark:text-neutral-100 inline-block hover:text-indigo-600 transition duration-500 ease-in-out"
+                    className="font-semibold text-sm md:text-md lg:text-md text-neutral-950 dark:text-neutral-100 inline-block hover:text-indigo-600 transition duration-500 ease-in-out"
                 >
-                    {product.title}
+                    {product.title.substring(0, 25)}
+                    {product.title.length > 25 ? "..." : ""}
                 </Link>
-                <p className="text-gray-500 dark:text-neutral-400 text-xs mt-2">
+                <p className="text-gray-500 dark:text-neutral-400 text-xs md:text-md lg:text-md mt-2">
                     {product.description.substring(0, 50)}
-                    ...
+                    {product.description.length > 50 ? "..." : ""}
                 </p>
             </div>
         </div>
