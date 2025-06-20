92d43f0da5874ea656d9b9a5c3e73836ddd66229
yustinus margiyuna <iyuszellda@gmail.com>
2025-06-20 12:26:53 +0700

    card fixing object cover and dark style

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function ProductCard({ product }) {
     const fallbackSrc = "https://placehold.co/200x200?text=Image+Not+Found";
     return (
-        <div className="bg-white rounded-lg overflow-hidden shadow group hover:shadow-xl transition duration-300 ease-in-out relative">
+        <div className="bg-white dark:bg-neutral-700 rounded-lg overflow-hidden shadow group hover:shadow-xl transition duration-300 ease-in-out relative">
             <Link to={`/demo/product/detail/${product.id}`} className="block">
-                <div className="relative w-full aspect-square bg-white">
-                    <div className="absolute top-0 right-0 text-white text-xs font-semibold px-3 py-1 rounded z-10">
+                <div className="relative w-full aspect-square bg-white dark:bg-neutral-700">
+                    <div className="absolute top-1 right-1 text-white text-xs font-semibold px-3 py-1 rounded z-10">
                         {product.category?.name}
                     </div>
                     <img
@@ -15,17 +15,17 @@ export default function ProductCard({ product }) {
                         onError={(e) => {
                             e.target.src = fallbackSrc;
                         }}
-                        className="w-full h-full object-contain group-hover:opacity-75"
+                        className="w-full h-full object-cover group-hover:opacity-75"
                     />
                     <div className="absolute bottom-2 left-2 bg-sky-600/90 text-white text-sm font-bold px-3 py-1 rounded">
                         ${product.price}
                     </div>
                 </div>
                 <div className="px-4 py-3">
-                    <h3 className="text-sm font-semibold text-neutral-900 truncate">
+                    <h3 className="text-sm font-semibold text-neutral-900 dark:text-neutral-100 truncate">
                         {product.title}
                     </h3>
-                    <p className="text-xs text-neutral-500 truncate">
+                    <p className="text-xs text-neutral-500 dark:text-neutral-200 truncate">
                         {product.description}
                     </p>
                 </div>
