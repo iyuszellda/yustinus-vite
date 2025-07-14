ca1db3fc721f64d9beddfe7a50ab67c68e08981b
yustinus margiyuna <iyuszellda@gmail.com>
2025-07-14 17:16:05 +0700

    endpoint api using laravel

--- src/lib/api/productApi.js
+++ src/lib/api/productApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const ProductApi = axios.create({
-    baseURL: "https://api.escuelajs.co/api/v1",
-    // baseURL: "http://localhost:8000/api/v1",
+    // baseURL: "https://api.escuelajs.co/api/v1",
+    baseURL: "https://api-product-i6yy.onrender.com/api/v1",
     timeout: 20000,
     headers: {
         "Content-Type": "application/json",
